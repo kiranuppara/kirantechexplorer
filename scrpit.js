39565c89147e39b278063889d3a7f5a4810b0f47
UPPARA SAI KIRAN <102009466+kiranuppara@users.noreply.github.com>
2023-09-02 19:52:35 +0530

    Add files via upload

---
+++ scrpit.js
@@ -0,0 +1,9 @@
+// document.getElementById("downloadbutton").addEventListener("click", function() {
+//     const link = document.createElement("a");
+//     link.href = "C:/Users/saiki/Desktop/WebDevelopment/Personal website/files/UPPARA_SAIKIRAN.pdf";
+//     link.download = "UPPARA_SAI_KIRAN_RESUME.pdf";
+//     document.body.appendChild(link);
+//     link.click();
+//     document.body.removeChild(link);
+//  });
+ 
